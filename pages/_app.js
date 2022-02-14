092863f8f10fd01485e7a9f1dfd7b5383fb3a7e8
Nick Kocharhook <nick@kocharhook.com>
2022-02-14 12:36:54 +0000

    Attempt to use pre-existing Cognito user pool.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import '@aws-amplify/ui-react/styles.css';
+
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: process.env.AWS_DEFAULT_REGION,
+        userPoolId: process.env.COGNITO_USER_POOL_ID,
+        userPoolWebClientId: process.env.COGNITO_CLIENT_ID,
+    }
+});
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withAuthenticator(MyApp)
